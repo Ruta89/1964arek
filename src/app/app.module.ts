a56091e9feb7fad0a733a4997492ef4b003cd77d
Arkadiusz Ruta <arek.ruta@gmail.com>
2017-12-06 16:55:58 +0100

    in module add MatTableModule, MatInputModule, MatButtonModule angularfire2 AngularFirestoreModule providers student, formsModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+
+import { environment } from '../environments/environment';
+
+import { MatTableModule, MatInputModule, MatButtonModule, MatButton, MatFormField, MatFormFieldModule } from '@angular/material';
+import { StudentService } from './student.service';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatInputModule,
+    MatTableModule,
+    MatFormFieldModule,
+    FormsModule,
+    AngularFireModule.initializeApp(environment.configFirebase),
+    AngularFirestoreModule.enablePersistence()
   ],
-  providers: [],
+  providers: [StudentService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
