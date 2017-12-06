aa8a3f5d08d854b248798e26a5e9854e49801930
Arkadiusz Ruta <arek.ruta@gmail.com>
2017-12-06 16:58:15 +0100

    StudentService: addStudent(d) getStudents()

---
+++ src/app/student.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { AngularFirestore } from 'angularfire2/firestore';
+@Injectable()
+export class StudentService {
+  constructor(private afs: AngularFirestore) {}
+
+  addStudent(studentData) {
+    this.afs
+      .collection('students')
+      .add(studentData)
+      .then(() => {
+        console.log('zrobione');
+      });
+  }
+
+  getStudents() {
+    return this.afs
+      .collection('students', ref => ref.orderBy('studentAge'))
+      .valueChanges();
+  }
+}
