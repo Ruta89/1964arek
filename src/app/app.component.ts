da522f6a7db73f12e854ee24761a0c57b4763a86
Arkadiusz Ruta <arek.ruta@gmail.com>
2017-12-06 17:00:09 +0100

    template: mat-form-field and mat-table

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
 import { Component } from '@angular/core';
-
+import { AngularFirestore } from 'angularfire2/firestore';
+import { StudentService } from './student.service';
+import { DataSource } from '@angular/cdk/collections';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+  studentDetails = {
+    studentName: '',
+    studentAge: '',
+    studentGrade: ''
+  };
+
+  dataSource = new StudentDataSource(this.student);
+  displayedColumns = ['Name', 'Age', 'Grade'];
+
+  constructor(private student: StudentService, private afs: AngularFirestore) {}
+
+  addStudent() {
+    this.student.addStudent(this.studentDetails);
+  }
+}
+
+export class StudentDataSource extends DataSource<any> {
+  constructor(private student: StudentService) {
+    super();
+  }
+  connect() {
+    return this.student.getStudents();
+  }
+
+  disconnect() {}
 }
