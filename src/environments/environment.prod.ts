e4de25046c388a85289487d628070f02a0f47574
Ruta89 <arek.ruta89@gmail.com>
2018-01-06 23:47:11 +0100

    hosting firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  configFirebase: {
+    apiKey: 'AIzaSyCNXEVJz_4Ev81EH11ofGTG-ABNEm5FkPw',
+    authDomain: 'testarek-d79f6.firebaseapp.com',
+    databaseURL: 'https://testarek-d79f6.firebaseio.com',
+    projectId: 'testarek-d79f6',
+    storageBucket: 'testarek-d79f6.appspot.com',
+    messagingSenderId: '221568232076'
+  }
 };
