933181d191465def7d2e1d2932cd86a40c48f50f
Ruta89 <arek.ruta89@gmail.com>
2018-01-07 23:48:35 +0100

    [fix] pwa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,7 @@ import { AngularFireModule } from 'angularfire2';
 import { AngularFirestoreModule } from 'angularfire2/firestore';
 
 import { environment } from '../environments/environment';
-// import { ServiceWorkerModule } from '@angular/service-worker';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 import { MaterialModule } from './shared/material.module';
 import { CzasService } from './shared/czas.service';
@@ -24,8 +24,8 @@ import { CzasService } from './shared/czas.service';
     FormsModule,
     MaterialModule,
     AngularFireModule.initializeApp(environment.configFirebase),
-    AngularFirestoreModule.enablePersistence()
-    // environment.production ? ServiceWorkerModule.register('ngsw-worker.js') : []
+    AngularFirestoreModule.enablePersistence(),
+    environment.production ? ServiceWorkerModule.register('ngsw-worker.js') : []
   ],
   providers: [CzasService],
   bootstrap: [AppComponent]
